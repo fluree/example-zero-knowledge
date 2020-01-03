1e85d50799844ed616bff61e3b8514dba50bf4b5
Lillia P <lpodlog@flur.ee>
2020-01-03 16:39:44 -0500

    initial commit

---
+++ src/circuits/InRange.circom
@@ -0,0 +1,52 @@
+include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/bitify.circom";
+
+template InAuthorizedRange(){
+
+    // Public inputs are the latitude and longitude ranges for an authorized fishing area.
+    signal input latitudeRange[2]; // Ranging from 0 to 180. i.e. (20, 21)
+    signal input longitudeRange[2]; // Ranging from 0 to 360 i.e. (76, 172)
+
+    // The private input is the location where the fish was caught.
+    // This is in the form (latitude, longitude)
+    signal private input fishingLocation[2];
+    signal output out; // 0 or 1
+
+    // Max num bits would be 9. 2^9 = 512, which is greater than max longitude range, 360. 
+
+    // The fishing latitude has to be greater than or equal to 
+    // the min latitude of the authorized area
+    component gt1 = GreaterEqThan(9);
+    gt1.in[0] <== fishingLocation[0];
+    gt1.in[1] <== latitudeRange[0];
+    gt1.out === 1; 
+
+    out <-- gt1.out;
+    
+
+    // // The fishing latitude has to be less than or equal to 
+    // // the max latitude of the authorized area
+    // component lt1 = LessEqThan(9);
+    // lt1.in[0] <== fishingLocation[0];
+    // lt1.in[1] <== latitudeRange[1];
+    // lt1.out === 1;
+
+    // // The fishing longitude has to be greater than or equal to 
+    // // the min longitude of the authorized area
+    // component gt2 = GreaterEqThan(9);
+    // gt2.in[0] <== fishingLocation[1];
+    // gt2.in[1] <== longitudeRange[0];
+    // gt2.out === 1;  
+
+    // // The fishing longitude has to be less than or equal to 
+    // // the max longitude of the authorized area
+    // component lt2 = LessEqThan(9);
+    // lt2.in[0] <== fishingLocation[1];
+    // lt2.in[1] <== longitudeRange[1];
+    // lt2.out === 1;  
+
+    // out <-- (gt1.out + gt2.out + lt1.out + lt2.out) * 1/4;
+    // out === 1;
+}
+
+component main = InAuthorizedRange();
---
+++ src/flureeFetch.js
@@ -0,0 +1,135 @@
+import fetch from 'isomorphic-fetch';
+
+function gateway(ip) {
+  let hosted = process.env.REACT_APP_ENVIRONMENT === "hosted";
+  let production = process.env.NODE_ENV === "production";
+
+  if (hosted && production) {
+    return "https://db.flur.ee";
+  } 
+  else if (hosted) {
+    return "http://localhost:8081"
+  } else if (!hosted){
+    return ip;
+  }
+}
+
+function getTokenClaims(token) {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+  catch (e) {
+    return {};
+  }
+}
+
+function parseJSON(response) {
+  return response.json().then(function (json) {
+    const newResponse = Object.assign(response, { json });
+
+	if (response.status < 300) {
+		return newResponse;
+	  } else {
+		throw newResponse;
+	  }
+  });
+}
+
+function fullEndpoint(endpoint, network, db, body, ip) {
+	const hosted = process.env.REACT_APP_ENVIRONMENT === "hosted";
+	const endpointInfix = hosted ? "api" : "fdb";
+
+	const locatedEndpoint = [
+		"query",
+		"multi-query",
+		"block",
+		"history",
+		"transact",
+		"graphql",
+		"sparql",
+		"command",
+		"snapshot"
+	].includes(endpoint);
+
+	const startURI = gateway(ip);
+
+	if (locatedEndpoint) {
+		if (endpoint === "snapshot") {
+			return `${startURI}/${endpointInfix}/${body["db/id"]}/${endpoint}`;
+		} else {
+			return `${startURI}/${endpointInfix}/${
+				hosted ? "db/" : ""
+			}${network}/${db}/${endpoint}`;
+		}
+	}
+
+	const prefixedEndpoints = [
+		"dbs",
+		"action",
+		"new-db",
+		"accounts",
+		"signin",
+		"health",
+		"sub",
+		"new-pw",
+		"reset-pw",
+		"activate-account",
+		"delete-db"
+	].includes(endpoint);
+
+	if (prefixedEndpoints) {
+		return `${startURI}/${endpointInfix}/${endpoint}`;
+	}
+
+	if (endpoint === "logs") {
+		return `${startURI}/${endpointInfix}/fdb/${endpoint}/${network}`;
+	}
+
+	throw {
+		status: 400,
+		message: "Invalid endpoint"
+	};
+}
+
+const flureeFetch = opts => {
+	// Opts include: ip, body, auth, network, db, endpoint
+	const { ip, body, auth, network, db, endpoint, headers, noRedirect } = opts;
+
+	const fullUri = fullEndpoint(endpoint, network, db, body, ip);
+
+	const finalHeaders = headers
+		? headers
+		: {
+				"Content-Type": "application/json",
+				"Request-Timeout": 20000,
+				Authorization: `Bearer ${auth}`
+		  };
+
+	const fetchOpts = {
+		method: "POST",
+		headers: finalHeaders,
+		body: JSON.stringify(body)
+	};
+
+	return fetch(fullUri, fetchOpts)
+		.then(parseJSON)
+		.catch(error => {
+			if (!noRedirect && (error.status === 401 || error.status === 403)) {
+				localStorage.removeItem("token");
+				// main token expired, need to log back in.
+				if (this.props) {
+					this.props.logout();
+				} else {
+					window.location = "/";
+				}
+			} else {
+				if (error.json) {
+					return error.json;
+				}
+
+				return error;
+			}
+		});
+};
+
+export { flureeFetch };
